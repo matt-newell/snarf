f7506f7472ef624b7575761a84f95581ef19b9c2
matt-newell <matt.newell@comcast.net>
2023-03-12 14:39:32 -0400

    add query command

---
+++ src/commands/snowflake/query.ts
@@ -0,0 +1,108 @@
+import { readFile } from 'fs/promises';
+import { Messages } from '@salesforce/core';
+import { SfCommand, Flags } from '@salesforce/sf-plugins-core';
+import * as snowflake from 'snowflake-sdk';
+
+// import {SnowflakeImportResult} from '../types'
+export type SnowflakeImportResult = {
+  account: string;
+  username: string;
+  sobject: string;
+  method: string;
+  extIdField: string;
+  query: string;
+  time: string;
+};
+
+Messages.importMessagesDirectory(__dirname);
+const messages = Messages.load('snarf', 'snowflake.query', [
+  'summary',
+  'description',
+  'examples',
+  'flags.account.summary',
+  'flags.username.summary',
+  'flags.query.summary',
+  'info.snowflake',
+]);
+
+export default class SnowflakeImport extends SfCommand<SnowflakeImportResult> {
+    public static readonly summary = messages.getMessage('summary');
+    public static readonly description = messages.getMessage('description');
+    public static readonly examples = messages.getMessages('examples');
+  
+    public static flags = {
+      account: Flags.string({
+        char: 'a',
+        summary: messages.getMessage('flags.account.summary'),
+        required: true
+      }),
+      username: Flags.string({
+        char: 'u',
+        summary: messages.getMessage('flags.username.summary'),
+        required: true
+      }),
+      query: Flags.string({
+        char: 'q',
+        summary: messages.getMessage('flags.query.summary')
+      }),
+    };
+
+    public async snowflakeConn(account:string, username:string, sql:string) {
+      const sqlQuery = await readFile(sql,{encoding:'utf8', flag:'r'})
+
+      const connection = snowflake.createConnection({
+        account: account,
+        username: username,
+        authenticator: "EXTERNALBROWSER",
+      })
+      connection.connectAsync()
+      .then(() => {
+        connection.execute({
+          sqlText: sqlQuery,
+          complete: (err, stmt, rows) => {
+            if (err) {
+              console.error(
+                "Failed to execute statement due to the following error: " +
+                err.message
+              )
+            } else {
+              // console.log("Number of rows produced: " + rows.length)
+              // return bulkv2(JSON.parse(JSON.stringify(rows)))
+              return rows
+            }
+          },
+        })
+      })
+    }
+
+    public async run(): Promise<SnowflakeImportResult> {
+      const { flags } = await this.parse(SnowflakeImport);
+      const time = new Date().toDateString();
+
+      const snowQuery = await this.snowflakeConn(
+        flags.account,
+        flags.username,
+        flags.query
+      )
+
+      this.log('bulkJob',snowQuery)
+      this.log(messages.getMessage('info.snowflake', [
+        flags.account, 
+        flags.username, 
+        flags.sobject, 
+        flags.method, 
+        flags.extIdField, 
+        flags.query, 
+      ]));
+      return {
+        account: flags.account,
+        username: flags.username,
+        sobject: flags.sobject,
+        method: flags.method,
+        extIdField: flags.extIdField,
+        query: flags.query,
+        time: time
+      };
+    }
+  }
+  
